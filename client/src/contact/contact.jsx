5a2c23f4e26d4c1fff9c620dbfae6280ef722365
KNiC.xo <ntcaliwag@hotmail.com>
2024-09-27 01:49:46 -0700

    Completed inner functionality for new contact

--- client/src/contact/contact.jsx
+++ client/src/contact/contact.jsx
@@ -10,7 +10,10 @@ function Contact() {
 
   let contactLists = JSON.parse(localStorage.getItem('contactLists')) || [];
   let {listName} = useParams();
-  let [displayName, setDisplayName] = useState(listName);
+  const [displayName, setDisplayName] = useState(listName);
+
+  const [phoneSlots, setPhoneSlots] = useState([]);
+  const [emailSlots, setEmailSlots] = useState([]);
 
   const testInsert = async () => {
     fetch('http://localhost:4001/insert', {
@@ -23,9 +26,118 @@ function Contact() {
     .then(data => console.log(data));
   }
 
+  function displayNewContact() {
+    return(
+      <>
+        <div className='new-contact'>
+          <div className='new-contact-header'>
+            <span className='new-contact-cancel' onClick={toggleNewContact}>Cancel</span>
+            <span className='new-contact-title'>New Contact</span>
+            <span className='new-contact-done'>Done</span>
+          </div>
+
+          <div className='new-contact-pfp'>
+            <img id='profile-picture' src="profile-picture.png" alt="" />
+            <input className='real-file-button' 
+                   onChange={displayPicture} 
+                   type="file" 
+                   accept='image/jpeg, image/jpg, image/png'
+            />
+            <span onClick={activateFileButton}>Add Photo</span>
+          </div>
+
+          <div className='name-company-div'>
+            <div className='new-contact-input-div'>
+              <input className='new-contact-input' type="text" placeholder='First name'/>
+            </div>
+
+            <div className='new-contact-input-div'>
+              <input className='new-contact-input' type="text" placeholder='Last name'/>
+            </div>
+
+            <div className='new-contact-input-div'>
+              <input className='new-contact-input' type="text" placeholder='Company'/>
+            </div>
+          </div>
+
+          {phoneSlots.map((_, index) => {
+            return(
+              <>
+                <div className='new-contact-input-div' key={index}>
+                  <img src="remove.png" className='remove-address' onClick={() => removePhoneSlots(index)} alt="" />
+
+                  <select className='address-select' name="phone-select">
+                    <option value="mobile">mobile</option>
+                    <option value="home">home</option>
+                    <option value="work">work</option>
+                  </select>
+
+                  <span className='select-arrow'>{`>`}</span>
+                  <input className='new-contact-input' id='phone' type="text" placeholder='Phone'/>
+                </div>
+              </>
+            );
+          })}
+
+          <div className='address-div'>
+            <img src="add-2.png" className='add-address' onClick={addPhoneSlots} alt="" />
+            <span className='new-contact-label'> add phone</span>
+          </div>
+
+          {emailSlots.map((_, index) => {
+            return(
+              <div className='new-contact-input-div' key={index}>
+                <img src="remove.png" className='remove-address' onClick={() => removeEmailSlots(index)} alt="" />
+                <input className='new-contact-input' id='email' type="text" placeholder='Email'/>
+              </div>
+            );
+          })}
+          
+          <div className='address-div'>
+            <img src="add-2.png" className='add-address' onClick={addEmailSlots} alt="" />
+            <span className='new-contact-label'> add email</span>
+          </div>
+
+          <div className='notes-div'>
+            <span>Notes</span>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  function addPhoneSlots() {
+    setPhoneSlots(prevSlots => [...prevSlots, {}]);
+  }
+
+  function removePhoneSlots(index) {
+    setPhoneSlots(prevSlots => prevSlots.filter((_, i) => i != index));
+  }
+
+  function addEmailSlots() {
+    setEmailSlots(prevSlots => [...prevSlots, {}]);
+  }
+
+  function removeEmailSlots(index) {
+    setEmailSlots(prevSlots => prevSlots.filter((_, i) => i != index));
+  }
+
   function toggleNewContact() {
     const newContact = document.querySelector('.new-contact');
     newContact.style.display = newContact.style.display === 'flex' ? 'none' : 'flex';
+  }
+
+  function activateFileButton() {
+    const realFileButton  = document.querySelector('.real-file-button');
+    realFileButton.click();
+  }
+
+  function displayPicture() {
+    const profilePicture = document.getElementById('profile-picture');
+    const realFileButton  = document.querySelector('.real-file-button');
+
+    profilePicture.src = URL.createObjectURL(realFileButton.files[0]);
   }
 
   function enableEdit() {
@@ -65,65 +177,7 @@ function Contact() {
   return(
     <>
       <div className='contact-wrapper'>
-        <div className='new-contact'>
-          <div className='new-contact-header'>
-            <span className='new-contact-cancel' onClick={toggleNewContact}>Cancel</span>
-            <span className='new-contact-title'>New Contact</span>
-            <span className='new-contact-done'>Done</span>
-          </div>
-
-          <div className='new-contact-pfp'>
-            <img src="profile-picture.png" alt="" />
-            <span>Add Photo</span>
-          </div>
-
-          <div className='name-company-div'>
-            <div className='new-contact-input-div'>
-              <input className='new-contact-input' type="text" placeholder='First name'/>
-            </div>
-
-            <div className='new-contact-input-div'>
-              <input className='new-contact-input' type="text" placeholder='Last name'/>
-            </div>
-
-            <div className='new-contact-input-div'>
-              <input className='new-contact-input' type="text" placeholder='Company'/>
-            </div>
-          </div>
-
-          <div className='new-contact-input-div'>
-            <img src="remove.png" className='remove-address' alt="" />
-
-            <select className='address-select' name="phone-select">
-              <option value="mobile">mobile</option>
-              <option value="home">home</option>
-              <option value="work">work</option>
-            </select>
-
-            <span className='select-arrow'>{`>`}</span>
-            <input className='new-contact-input' id='phone' type="text" placeholder='Phone'/>
-          </div>
-
-          <div className='address-div'>
-            <img src="add-2.png" className='add-address' alt="" />
-            <span className='new-contact-label'> add phone</span>
-          </div>
-          
-          <div className='new-contact-input-div'>
-            <img src="remove.png" className='remove-address' alt="" />
-            <input className='new-contact-input' id='email' type="text" placeholder='Email'/>
-          </div>
-
-          <div className='address-div'>
-            <img src="add-2.png" className='add-address' alt="" />
-            <span className='new-contact-label'> add email</span>
-          </div>
-
-          <div className='notes-div'>
-            <span>Notes</span>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-        </div>
+        {displayNewContact()}
 
         <div className='contact-header-buttons'>
           <Link className='list-link'
